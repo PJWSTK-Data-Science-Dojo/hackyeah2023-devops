a59cf8bebe1bee24ed395fae4a85cbd34452a016
Pexo <contact@pexogamedev.pl>
2023-10-01 11:49:26 +0200

    FIXED

--- complete-setup-from-local-repos-windows.bat
+++ complete-setup-from-local-repos-windows.bat
@@ -14,12 +14,15 @@ docker build -t %API_IMAGE_NAME% %API_PROJECT_PATH%
 
 echo Building UI Docker image...
 docker build -t %UI_IMAGE_NAME% %UI_PROJECT_PATH%
-
 echo Building ML Project Docker image...
 docker build -t %ML_IMAGE_NAME% %ML_PROJECT_PATH%
-
 REM Use Docker Compose to start the services
 echo Starting services with Docker Compose...
-docker-compose -f .\docker-compose-local.yml up -d
+docker-compose -f .\docker-compose-local.yml up --build -d
+
+@REM docker-compose -f %ML_DOCKER_COMPOSE_PATH% down
+@REM docker-compose -f %ML_DOCKER_COMPOSE_PATH% up -d
+@REM docker run -d --name hackyeah-api -p 5012:80 hackyeah2023-api-local:latest
+@REM docker run -d --name hackyeah-ui -p 4200:80 hackyeah2023-ui-local:latest
 
 echo All services are up and running!
